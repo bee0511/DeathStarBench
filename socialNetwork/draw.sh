b87604764cd83f61286d666276c5fcaf2a104930
bee0511 <bee000092.cs10@nycu.edu.tw>
2025-04-26 18:34:58 -0600

    add draw script

---
+++ socialNetwork/draw.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+THREAD_COUNT=8
+CONNECTION_COUNT=50
+DURATION=300s
+REQUEST_RATE=50
+
+python hdr-plot.py \
+    results/rr_c${CONNECTION_COUNT}_d${DURATION}_r${REQUEST_RATE}.log \
+    results/po2_c${CONNECTION_COUNT}_d${DURATION}_r${REQUEST_RATE}.log \
+    results/prequal_c${CONNECTION_COUNT}_d${DURATION}_r${REQUEST_RATE}.log \
+    --output results/image.png
